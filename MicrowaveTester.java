a9d94bd70b376c7c280ed781c4dc0180434baf59
Zak <91210754+Zak929@users.noreply.github.com>
2022-09-29 15:14:13 -0400

    Microwave Assignment

---
+++ MicrowaveTester.java
@@ -0,0 +1,13 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		Microwave myMicrowave = new Microwave();
+		myMicrowave.timeButton();
+		myMicrowave.timeButton();
+		myMicrowave.powerButton();
+		myMicrowave.start();
+		myMicrowave.reset();
+	}
+
+}
